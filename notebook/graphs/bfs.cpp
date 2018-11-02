7624c9192e95ef7766a129c39d09e0c8f71ea940
navarro <ricksnavarro@gmail.com>
2018-11-02 16:50:18 -0300

    Added graphs codes

---
+++ notebook/graphs/bfs.cpp
@@ -0,0 +1,18 @@
+// Breadth First Search O(V+E)
+
+const int N = 1e5 + 5;
+
+int vis[N];
+vi adj[N];
+queue<int> q;
+
+void bfs(int x){
+    vis[x] = 1;
+    q.push(x);
+    while(!q.empty){
+        for(auto u : adj[x]) if(!vis[u]){
+            vis[u] = 1;
+            q.push(v);
+        }
+    }
+}
