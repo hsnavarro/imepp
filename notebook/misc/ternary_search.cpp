4ad7ede1f1d183fb0258c829aaa9bc4552043417
hsnavarro <ricksnavarro@gmail.com>
2019-05-05 11:26:56 -0300

    Updated ternary search

--- notebook/misc/ternary_search.cpp
+++ notebook/misc/ternary_search.cpp
@@ -1,26 +1,22 @@
 // Ternary Search - O(log n)
 
 int ternary_search_min (int l = 1, int r = N){
-    int m1, m2, mid, mn = INF, idx;
+    int mid, mn = INF, idx;
     while(r - l > 5){
         mid = (l+r)/2;
-        m1 = mid;
-        m2 = mid + 1;
-        if(func(m2) < func(m1)) l = m1;
-        else r = m2;
+        if(func(mid + 1) < func(mid)) l = mid;
+        else r = mid + 1;
     }
     for(int i = l; i <= r; i++) if(func(i) < mn) mn = func(i), idx = i;
     return idx;
 }
 
 int ternary_search_max (int l = 1, int r = N){
-    int m1, m2, mid, mx = -INF, idx;
+    int mid, mx = -INF, idx;
     while(r - l > 5){
         mid = (l+r)/2;
-        m1 = mid;
-        m2 = mid + 1;
-        if(func(m2) > func(m1)) l = m1;
-        else r = m2;
+        if(func(mid + 1) > func(mid)) l = mid;
+        else r = mid + 1;
     }
     for(int i = l; i <= r; i++) if(func(i) > mx) mx = func(i), idx = i;
     return idx;
