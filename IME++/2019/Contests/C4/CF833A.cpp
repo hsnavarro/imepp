240b5d042da97db337033becb680c1023b3cfb2e
hsnavarro <ricksnavarro@gmail.com>
2019-05-06 22:45:47 -0300

    Updated C4 Question

--- IME++/2019/Contests/C4/CF833A.cpp
+++ IME++/2019/Contests/C4/CF833A.cpp
@@ -3,17 +3,15 @@ using namespace std;
 
 typedef long long ll;
 
-int n;
-ll a, b;
+int n, a, b, cube;
 double EPS = 1e-9;
 
 int main(){
-  ios_base::sync_with_stdio(0), cin.tie(0);
   scanf("%d", &n);
   while(n--){
-    scanf("%lld %lld", &a, &b);
-    ll cube = cbrt(a*b) + EPS;
-    if(cube*cube*cube == a*b and !(a%cube) and !(b%cube)) printf("Yes\n");
-    else printf("No\n");
+    scanf("%d%d", &a, &b);
+    cube = cbrt(1ll*a*b) + EPS;
+    1ll*cube*cube*cube == 1ll*a*b and !(a%cube) and !(b%cube) ? 
+      printf("Yes\n") : printf("No\n");
   }
 }
