6585c5b1db42f91fab090a89c0d90eb23460bc33
hsnavarro <ricksnavarro@gmail.com>
2020-01-30 16:58:41 -0400

    Added String Hashing

--- notebook/strings/string-hashing.cpp
+++ notebook/strings/string-hashing.cpp
@@ -1,20 +1,43 @@
-// String Hashing - O(n + m)
+// String Hashing
+// Rabin Karp - O(n + m)
+
+// max size txt + 1
+const int N = 1e6 + 5;
+
+// lowercase letters p = 31
+// uppercase letters p = 53
+// any character p = 313
+
 const int MOD = 1e9+9;
-char s[N];
-int n; // n = strlen(s)
-ll sum[N], p[N+1] = {1, 313};
+ll h[N], p[N] = {1, 31};
+int cnt;
 
-void build(char* s){
-  sum[0] = s[0];
-  for(int i = 1; i < n; i++) sum[i] = (sum[i-1]*p[1]+s[i])%MOD;
-  for(int i = 2; i <= N; i++) p[i] = p[1]*p[i-1]%MOD;
+void build(string &s) {
+  for(int i = 1; i <= s.size(); i++) {
+    h[i] = ((p[1]*h[i-1]) % MOD + s[i-1]) % MOD;
+    p[i] = (p[1]*p[i-1]) % MOD;
+  }
 }
 
-int fhash(int l = 0, int r = n-1){
-  return p[N]*(((sum[r] - sum[l-1]*p[r-l+1]%MOD)+MOD)%MOD)%MOD;
+// 1-indexed
+ll fhash(int l, int r) {
+  return (h[r] - ((h[l-1]*p[r-l+1]) % MOD) + MOD) % MOD;
 }
 
-int hashsimple(char* s, int l = 0, int r = n-1, ll ans = 0){
-  for(int i = l; i <= r; i++) ans = (ans + p[r-i]*s[i]%MOD)%MOD;
-  return p[N]*ans%MOD;
+ll shash(string &pt) {
+  ll h = 0;
+  for(int i = 0; i < pt.size(); i++) 
+    h = ((h*p[1]) % MOD + pt[i]) % MOD;
+  return h;
+}
+
+void rabin_karp(string &s, string &pt) {
+  build(s);
+  ll hp = shash(pt);
+  for(int i = 0, m = pt.size(); i + m <= s.size(); i++) {
+    if(fhash(i+1, i+m) == hp) {
+      // match at i
+      cnt++;
+    }
+  }
 }
