82dee0374ed60ea41b3f6d8973c873a62a8911fe
hsnavarro <ricksnavarro@gmail.com>
2020-04-02 19:01:26 -0300

    Updated automaton

--- notebook/strings/kmp-automaton.cpp
+++ notebook/strings/kmp-automaton.cpp
@@ -3,17 +3,9 @@
 // max size pattern
 const int N = 1e5 + 5;
 
-int cnt, lps[N], nxt[N+1][26];
+int cnt, nxt[N+1][26];
 
 void prekmp(string &p) {
-  /* If needed lps values 
-  for(int i = 1, j = 0; i < p.size(); i++) {
-    while(j and p[j] != p[i]) j = lps[j-1];
-    if(p[j] == p[i]) j++;
-    lps[i] = j;
-  }
-  */
-
   nxt[0][p[0] - 'a'] = 1;
   for(int i = 1, j = 0; i <= p.size(); i++) {
     for(int c = 0; c < 26. c++) nxt[i][c] = nxt[j][c];
