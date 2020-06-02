b4d9b36774e3e2ef73d9f0083a22ec2e2f550c7d
Henrique Soldi Navarro <42014408+hsnavarro@users.noreply.github.com>
2020-06-02 09:41:46 -0300

    Update kuhn.cpp

--- notebook/graphs/kuhn.cpp
+++ notebook/graphs/kuhn.cpp
@@ -2,10 +2,10 @@
 
 int n, cnt, vis[N], match[N], ans;
 
-bool find(int x){
-  if(vis[x] == cnt) return false;
-  vis[x] = cnt;
-  for(auto u : adj[x]) if(!match[u] or find(match[u])) return match[u] = x;
+bool find(int u){
+  if(vis[u] == cnt) return false;
+  vis[u] = cnt;
+  for(auto v : adj[u]) if(!match[v] or find(match[v])) return match[v] = x;
   return false;
 }
 
