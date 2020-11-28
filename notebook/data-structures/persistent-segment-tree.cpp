3db93ecad2d138882f5dd402346378db8f623ce7
hsnavarro <ricksnavarro@gmail.com>
2020-11-28 19:03:36 -0300

    Update Persistent SegTree

--- notebook/data-structures/persistent-segment-tree.cpp
+++ notebook/data-structures/persistent-segment-tree.cpp
@@ -1,30 +1,53 @@
 //Persistent Segment Tree
 //Update and Query - O(log n)
-//Space - O(n log n)
+//Space - O(n log n + q * log n)
 
-//M -> n log n
+//M -> n log n + q * log n
 
-int nxt;
-int lc[M], rc[M], st[M];
-vector<int> root;
+const int N = 2e5 + 5;
 
-int update(int p, int l, int r, int idx, int v){
-    if(l == r) { st[nxt] = v; return nxt++; }
-    int mid = (l+r)/2;
-    int u = nxt++;
-    if(i <= mid){
-        lc[u] = update(lc[p], l, mid, idx, v);
-        rc[u] = rc[p];
-    }else{
-        rc[u] = update(rc[p], mid, r, idx, v);
-        lc[u] = lc[p];
-    }
-    st[u] = st[lc[u]] + st[rc[u]]; // RMQ -> min/max, RSQ -> +
-    return u;
+const int M = 1e7 + 5;
+
+int n;
+int st[M], lc[M], rc[M];
+int cnt, v[N];
+
+void init(int p = 0, int l = 1, int r = n) {
+    if (l == r) { st[p] = v[l]; return; }
+
+    int mid = (l + r) / 2;
+
+    lc[p] = ++cnt;
+    rc[p] = ++cnt;
+
+    init(lc[p], l, mid);
+    init(rc[p], mid + 1, r);
+
+    st[p] = st[lc[p]] + st[rc[p]];
 }
 
-int query(int p, int l, int r, int i, int j){
-    if(i > r or l > j) return 0;
-    if(i <= l and j >= r) return st[p];
-    return query(lc[p], l, (l+r)/2, i, j) + query(rc[p], (l+r)/2 + 1, r, i, j); // RMQ -> min/max, RSQ -> +
+int query(int i, int j, int p, int l = 1, int r = n) {
+    if (r < i or l > j) return 0;
+    if (i <= l and r <= j) return st[p];
+
+    int mid = (l + r) / 2;
+    return query(i, j, lc[p], l, mid) + query(i, j, rc[p], mid + 1, r);
+}
+
+int update(int idx, int val, int p, int l = 1, int r = n) {
+    int u = ++cnt;
+    if (l == r) st[u] = val;
+    else {
+      int mid = (l + r) / 2;
+      if (idx <= mid) {
+        lc[u] = update(idx, val, lc[p], l, mid);
+        rc[u] = rc[p];
+      } else {
+        lc[u] = lc[p];
+        rc[u] = update(idx, val, rc[p], mid + 1, r);
+      }
+    }
+
+    st[u] = st[lc[u]] + st[rc[u]];
+    return u;
 }
