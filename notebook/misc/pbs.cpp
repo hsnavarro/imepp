5d78c2445db7fbd977033bc46eabbbb05a6351a2
Henrique Navarro <ricksnavarro@gmail.com>
2021-08-17 22:52:29 -0400

    Refactor code

--- notebook/misc/pbs.cpp
+++ notebook/misc/pbs.cpp
@@ -1,33 +1,34 @@
-//Parallel Binary Search - O(nlog^2 n + q)
+// Parallel Binary Search - O(nlog n * cost to update data structure + qlog n * cost for bs condition)
 
-struct Query{ int l, r, i, ans;};
-vector<Query> req;
+struct query { int i, ans; /*+ query related info*/ };
+vector<query> req;
+int l_default, r_default, idx = l_default;
 
-void pbs2(int L, int R, vector<Query> Q){
-    int mid = (L+R)/2;
+void pbs(vector<query>& qs, int l_pbs = l_default, int r_pbs = r_default){
+    int mid = (l_pbs + r_pbs) / 2;
     // mid = (L+R+1)/2 if different from simple upper/lower bound
-    if(Q.empty()) return;
-    while(i < mid) {
-        i++;
+    if(qs.empty()) return;
+
+    while(idx < mid) {
+        idx++;
         //add value to data structure
     }
-    while(i > mid) {
+    while(idx > mid) {
         //remove value to data structure
-        i--;
+        idx--;
     }
 
-    if(L == R) {
-        for(auto q : Q) req[q.i].ans = L;
+    if(l_pbs == r_pbs) {
+        for(auto q : qs) req[q.i].ans = l_pbs;
         return;
     }
 
-    vector<Query> vl, vr;
-    for (auto q : Q) {
+    vector<query> vl, vr;
+    for (auto& q : qs) {
         if ( /* cond */ ) vl.pb(q);
         else vr.pb(q);
     }
-    pbs(L, mid, vl);
-    pbs(mid + 1, R, vr);
+
+    pbs(vl, l_pbs, mid);
+    pbs(vr, mid + 1, r_pbs);
 }
-
-
